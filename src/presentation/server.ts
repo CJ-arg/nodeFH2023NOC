94b625f92d0c7c41c39cf10eda93535ab602a830
CJ-arg <janoncarlos@gmail.com>
2024-01-23 16:56:07 -0300

    config

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,16 @@
+import { CronJob } from "cron";
+
 export class Server {
   public static start() {
     console.log("Server started...");
+
+    const job = new CronJob(
+      "*/20 * * * * *", // cronTime
+      () => {
+        const date = new Date();
+        console.log("You will see this message every 20 second", date);
+      } // onTick
+    );
+    job.start();
   }
 }
