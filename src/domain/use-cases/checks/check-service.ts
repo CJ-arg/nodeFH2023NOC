d890da5ee01feff1bc123062972dd6f4e394e2e7
CJ-arg <janoncarlos@gmail.com>
2024-01-25 16:47:14 -0300

    cron execute url

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -15,10 +15,12 @@ export class CheckService implements CheckServiceUseCase {
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
+      this.successCallback();
       console.log(`${url} is OK`);
 
       return true;
     } catch (error) {
+      this.errorCallback(`${error}`);
       console.log(`${error}`);
 
       return false;
