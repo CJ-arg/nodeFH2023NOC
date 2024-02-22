b1478e0567c87d9d00b40df5356c6c389a67ec24
CJ-arg <janoncarlos@gmail.com>
2024-02-22 17:47:03 -0300

    logs file system

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -1,12 +1,21 @@
 import { LogDatasource } from "../../domain/datasources/log.datsource";
 import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
-
+import fs from "fs";
 export class FileSystemDatasource implements LogDatasource {
   private readonly logPath = "losgs/";
   private readonly lowLogsPath = "losgs/logs-low.log";
   private readonly midLogsPath = "losgs/logs-medium.log";
   private readonly hiLogsPath = "losgs/logs-high.log";
 
+  constructor() {
+    this.createLogsFiles();
+  }
+
+  private createLogsFiles = () => {
+    if (!fs.existsSync(this.logPath)) {
+      fs.mkdirSync(this.logPath);
+    }
+  };
   saveLog(log: LogEntity): Promise<void> {
     throw new Error("Method not implemented.");
   }
