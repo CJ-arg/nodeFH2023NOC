a70793c0d5a76542139e86026e59a048aa13f3ed
CJ-arg <janoncarlos@gmail.com>
2024-02-22 17:27:42 -0300

    readme

--- src/domain/repository/log.repository.ts
+++ src/domain/repository/log.repository.ts
@@ -1,6 +1,7 @@
 import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
 
 export abstract class LogRepository {
+  datasource;
   abstract saveLog(log: LogEntity): Promise<void>;
   abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
 }
